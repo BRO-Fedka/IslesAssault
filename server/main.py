56633223271c6c7d658551566089e2646a31153f
BRO-Fedka <71376448+BRO-Fedka@users.noreply.github.com>
2024-08-08 18:23:09 +0300

    server json map

--- server/main.py
+++ server/main.py
@@ -273,82 +273,44 @@ JSVEHs = os.environ['JS_VEH']
 # MAPJSON = open('../httpserver/static/MAP.json', 'r').read()
 MAPJSON = os.environ['JSON_MAP']
 # print(MAPJSON)
-MAP = pickle.load(open('MAP', 'rb'))
+MAP = None
+with open('MAP.json', 'r') as file:
+    MAP = json.load(file)
+    print(MAP)
 MAPobjSIDEdir = {'S': {}, 'B': {}}
 # print(MAP['B'])
 i = 0
-while i in range(0, len(MAP['B'])):
-    # print(i)
-    if MAP['B'][i] == []:
-        MAP['B'].pop(i)
-        i -= 1
-    i += 1
-i = 0
-while i in range(0, len(MAP['S'])):
-    # print(i)
-    if MAP['S'][i] == []:
-        MAP['S'].pop(i)
-        i -= 1
-    i += 1
-if True:
-    for i in range(0, len(MAP['S'])):
-        # print(MAP['S'][i])
+WH = MAP['WH']
+for i in range(0, len(MAP['S'])):
+    srtg = (Polygon(
+        LinearRing(MAP['S'][i]).parallel_offset(0.01, 'right', join_style=1, resolution=1).coords))
+    MAPobjSIDEdir['S'][i] = True
+    if srtg.area < Polygon(MAP['S'][i]).area:
+        MAPobjSIDEdir['S'][i] = False
+    MAP['S'][i] = Polygon(MAP['S'][i])
 
-        srtg = (Polygon(
-            LinearRing(MAP['S'][i].exterior.coords).parallel_offset(0.01, 'right', join_style=1, resolution=1).coords))
-        MAPobjSIDEdir['S'][i] = True
-        if srtg.area < MAP['S'][i].area:
-            MAPobjSIDEdir['S'][i] = False
-if len(MAP['Z']) == 0:
-    for i in range(0, len(MAP['B'])):
-        # print(list(MAP['B'][i].exterior.coords))
+for i in range(0, len(MAP['B'])):
+    srtg = (Polygon(
+        LinearRing(MAP['B'][i]).parallel_offset(0.01, 'right', join_style=1, resolution=1).coords))
+    MAPobjSIDEdir['B'][i] = True
+    if srtg.area < Polygon(MAP['B'][i]).area:
+        MAPobjSIDEdir['B'][i] = False
+    MAP['B'][i] = Polygon(MAP['B'][i])
 
-        MAP['Z'].append(Polygon(
-            LinearRing(MAP['B'][i].exterior.coords).parallel_offset(0.25, 'right', join_style=1, resolution=1).coords))
-        MAPobjSIDEdir['B'][i] = True
-        if MAP['Z'][i].area < MAP['B'][i].area:
-            MAP['Z'][i] = Polygon(LinearRing(MAP['B'][i].exterior.coords).parallel_offset(0.25, 'left', join_style=1,
-                                                                                          resolution=1).coords)
-            MAPobjSIDEdir['B'][i] = False
-if len(MAP['G']) == 0:
-    for i in range(0, len(MAP['B'])):
-        # print(list(MAP['B'][i].exterior.coords))
+Q = MAP['Q']
+MAP["Q"] = {}
+for x in range(0, WH):
+    for y in range(0, WH):
+        MAP["Q"][(x,y)] = Q[x][y]
 
-        MAP['G'].append(Polygon(
-            LinearRing(MAP['B'][i].exterior.coords).parallel_offset(0.1, 'left', join_style=1, resolution=1).coords))
-        if MAP['G'][i].area > MAP['B'][i].area:
-            MAP['G'][i] = Polygon(LinearRing(MAP['B'][i].exterior.coords).parallel_offset(0.1, 'right', join_style=1,
-                                                                                          resolution=1).coords)
 
 # print(len(MAP['Z']),len(MAP['B']))
-Bridges = {0: [7.31, 4.63, 8.51, 4.63], 1: [5.24, 5.43, 5.78, 4.77], 2: [8.83, 4.88, 9.21, 5.53],
-           3: [12.55, 6.22, 12.58, 7.07]}
-for _ in Bridges.keys():
+Bridges = MAP['_']
+for _ in range(0, len(Bridges)):
     line = LineString([(Bridges[_][0], Bridges[_][1]), (Bridges[_][2], Bridges[_][3])])
     left_hand_side = line.buffer(0.1875 / 2, single_sided=True)
     right_hand_side = line.buffer(-0.1875 / 2, single_sided=True)
     Bridges[_].append(left_hand_side.union(right_hand_side))
-MAP['Q'] = {}
-for x in range(0, WH):
-    for y in range(0, WH):
-        MAP['Q'][(x, y)] = {'p': Polygon([[x, y], [x + 1, y], [x + 1, y + 1], [x, y + 1]]), 'B': [],
-                            'Z': [], 'G': [], 'S': [], 'BRIDGES': []}
-        for _0 in range(0, len(MAP['B'])):
-            if type(MAP['B'][_0]) != type([]) and MAP['Q'][(x, y)]['p'].intersects(MAP['B'][_0]):
-                MAP['Q'][(x, y)]['B'].append(_0)
-                # print(_0)
-        for _0 in range(0, len(MAP['Z'])):
-            if type(MAP['Z'][_0]) != type([]) and MAP['Q'][(x, y)]['p'].intersects(MAP['Z'][_0]):
-                MAP['Q'][(x, y)]['Z'].append(_0)
-        for _0 in range(0, len(MAP['G'])):
-            if type(MAP['G'][_0]) != type([]) and MAP['Q'][(x, y)]['p'].intersects(MAP['G'][_0]):
-                MAP['Q'][(x, y)]['G'].append(_0)
-        for _0 in range(0, len(MAP['S'])):
-            if type(MAP['S'][_0]) != type([]) and MAP['Q'][(x, y)]['p'].intersects(MAP['S'][_0]):
-                MAP['Q'][(x, y)]['S'].append(_0)
-        for _0 in range(0, len(Bridges)):
-            if MAP['Q'][(x, y)]['p'].intersects(Bridges[_0][4]):
-                MAP['Q'][(x, y)]['BRIDGES'].append(_0)
 
 for x in range(0, WH):
     for y in range(0, WH):
@@ -358,9 +320,11 @@ for x in range(0, WH):
         MAP['Q'][(x, y)]['SMOKES'] = set()
         MAP['Q'][(x, y)]['AAROCKETS'] = set()
         MAP['Q'][(x, y)]['BOMBS'] = set()
-Zones = {'A': [8, 8, '', '', 0, 1],
-         'B': [4.35, 7.5, '', '', 0, 1],
-         }  ### X,Y,p/t,name,perc
+print(MAP['*'])
+Zones = {}  ### X,Y,p/t,name,perc
+for _ in MAP["*"]:
+    Zones[_[0]] = [_[1], _[2], '', '', 0, _[3]]
+print(Zones)
 PlayersSockets = {}
 PlayersInputs = {}
 PlayersData = {}
@@ -1699,7 +1663,7 @@ async def game():
                                 PlayersData[player]['VISBMB'].add(_)
                             for _ in MAP['Q'][(x, y)]['SMOKES']:
                                 PlayersData[player]['VISSMK'].add(_)
-                            for _ in MAP['Q'][(x, y)]['BRIDGES']:
+                            for _ in MAP['Q'][(x, y)]['_']:
                                 PlayersData[player]['VISBRIDGES'].add(_)
                             MAP['Q'][(x, y)]['PLAYERS'].add(player)
                         except:
